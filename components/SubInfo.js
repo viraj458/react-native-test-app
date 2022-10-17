e227d533ef014b8d5cf4b4afc5eb0078e8c16c59
Viraj <virajmalaka94@gmail.com>
2022-10-17 18:39:37 +0530

    Add EndDate component

--- components/SubInfo.js
+++ components/SubInfo.js
@@ -42,8 +42,26 @@ export const People = () => {
 }
 export const EndDate = () => {
     return (
-      <View>
-        <Text>EndDate</Text>
+      <View style={{
+        paddingHorizontal: SIZES.font,
+        paddingVertical: SIZES.base,
+        backgroundColor:COLORS.white,
+        justifyContent: 'center',
+        alignItems:'center',
+        ...SHADOWS.light,
+        elevation: 1,
+        maxWidth: '50%'
+      }}>
+        <Text style={{
+            fontFamily: FONTS.regular,
+            fontSize: SIZES.small,
+            color: COLORS.primary,
+        }}>Ending in</Text>
+        <Text style={{
+            fontFamily: FONTS.semiBold,
+            fontSize: SIZES.medium,
+            color: COLORS.primary,
+        }}>12h 30m</Text>
       </View>
     )
 }
