365e225f4d499c7493c70e4521f747b5f01f4a94
Viraj <virajmalaka94@gmail.com>
2022-10-17 15:54:27 +0530

    Create home

---
+++ components/Button.js
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native'
+
+
+export const CircleButton = () => {
+  return (
+    <View>
+      <Text>Button</Text>
+    </View>
+  )
+}
+
+export const RectButton = () => {
+    return (
+      <View>
+        <Text>Circle</Text>
+      </View>
+    )
+  }
+  
+
